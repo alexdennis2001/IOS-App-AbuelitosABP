e81c7e088d8bab06bf4218ff456b47612bb8f766
alexdennis2001 <80014741+alexdennis2001@users.noreply.github.com>
2022-09-08 12:14:05 -0500

    AbuelitosABP App

---
+++ AbuelitosABP/AbuelitosABP/AbuelitosABPApp.swift
@@ -0,0 +1,20 @@
+//
+//  AbuelitosABPApp.swift
+//  AbuelitosABP
+//
+//  Created by Alejandro Dennis on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AbuelitosABPApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
