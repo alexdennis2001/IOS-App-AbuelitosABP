02a95aada28be945eb798db8badf3dc408cc42f3
ArturoPatio23 <A00828096@tec.mx>
2022-10-04 22:08:04 -0500

    Arturo-FormularioDP-Update
    Creación de la vista de formulario datos personales.

---
+++ AbuelitosABP/AbuelitosABP/Home/DropdownRow.swift
@@ -0,0 +1,36 @@
+//
+//  DropdownRow.swift
+//  AbuelitosABP
+//
+//  Created by Arturo on 03/10/22.
+//
+
+import SwiftUI
+
+struct DropdownRow: View {
+    var option: DropdownOption
+    var onOptionSelected: ((_ option: DropdownOption) -> Void)?
+    
+    var body: some View {
+        Button(action: {
+            if let onOptionSelected = self.onOptionSelected {
+                onOptionSelected(self.option)
+            }
+        }) {
+            HStack {
+                Text(self.option.value)
+                    .font(.system(size: 14))
+                    .foregroundColor(Color.black)
+                Spacer()
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 5)
+    }
+}
+
+/*struct DropdownRow_Previews: PreviewProvider {
+    static var previews: some View {
+        DropdownRow()
+    }
+}*/
