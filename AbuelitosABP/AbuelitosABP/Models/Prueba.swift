f2f0e19b812c86ec788418ee32848059c7c730b1
ArturoPatio23 <A00828096@tec.mx>
2022-10-16 12:47:40 -0500

    Arturo-Prueba-Update

--- AbuelitosABP/AbuelitosABP/Models/Prueba.swift
+++ AbuelitosABP/AbuelitosABP/Models/Prueba.swift
@@ -7,8 +7,21 @@
 
 import SwiftUI
 
-struct Prueba: Identifiable {
+class Prueba: ObservableObject, Identifiable {
     var id = UUID()
     var nombre: String
     var preg_resp: [PregResp]
+    var rangosScore: [[Double]]
+    var orden: Int
+    var categorias: [String]
+    var scoreFinal: Double
+    
+    init(nombre: String, preg_resp: [PregResp], rangosScore: [[Double]], orden: Int, categorias: [String], scoreFinal: Double){
+        self.nombre = nombre
+        self.preg_resp = preg_resp
+        self.rangosScore = rangosScore
+        self.orden = orden
+        self.categorias = categorias
+        self.scoreFinal = scoreFinal
+    }
 }
