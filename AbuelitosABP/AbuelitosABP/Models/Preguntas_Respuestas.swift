e477799c13b34d40ecc8d0cf7c0e5f42546d5048
ArturoPatio23 <A00828096@tec.mx>
2022-10-19 21:24:49 -0500

    Arturo-ScoreFinal-Update6

---
+++ AbuelitosABP/AbuelitosABP/Models/Preguntas_Respuestas.swift
@@ -0,0 +1,14 @@
+//
+//  Preguntas_Respuestas.swift
+//  AbuelitosABP
+//
+//  Created by Arturo on 19/10/22.
+//
+
+import SwiftUI
+
+struct Preguntas_Respuestas: Identifiable{
+    var id = UUID()
+    let question_id: Int
+    let answer: Double
+}
---
+++ AbuelitosABP/AbuelitosABP/Models/PruebasScore.swift
@@ -0,0 +1,23 @@
+//
+//  PruebaScoreModel.swift
+//  AbuelitosABP
+//
+//  Created by Arturo on 19/10/22.
+//
+
+import SwiftUI
+
+struct PruebasScore: Codable {
+    let test_id: Int
+    let score: Double
+    let questions_answers: [Questions_Answers]
+    
+    struct Questions_Answers: Codable{
+        let question_id: Int
+        let answer: Double
+    }
+}
+
+
+
+
